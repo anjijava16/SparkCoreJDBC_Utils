5059ec18fde1af2b9120e42456cba7043160bae0
Anjaiah Methuku <anjijava16@users.noreply.github.com>
2021-01-11 16:44:24 -0500

    Create run_commands.sh

---
+++ run_commands.sh
@@ -0,0 +1,2 @@
+# Spark Run_Command:
+spark-submit --master local[*] --class com.mts.matrix.spark.main.StreamObjectMain mts-matrix-spark-utils-1.0-SNAPSHOT-jar-with-dependencies.jar JOB_NAME C:/data/welcome1.properties 200 400
